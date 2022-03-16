3cd39154c0e2adc3e98786c1c5ad6fc77081eaf5
Maciej <alien125@g.pl>
2022-03-16 23:10:57 +0100

    Changing class Card to DataClass. Transferring the game logic to the Game class. Corrections in the calculation of points.

--- black_jack.py
+++ black_jack.py
@@ -4,6 +4,7 @@
 
 """Backjack game simulator."""
 
+from dataclasses import dataclass, field
 from random import shuffle
 from card_settings import color_list, figure_list, value_list
 
@@ -19,16 +20,19 @@ class OverTwentyOne(Exception):
 class BlackJack(Exception):
     """Exception when player has 21 points"""
 
+@dataclass(repr=False, frozen=True)
 class Card:
     """A class that describes a playing card."""
 
-    def __init__(self, color, figure) -> None:
-        self.color = color
-        self.figure = figure
-        self.value = value_list[figure_list.index(figure)]
+    color : str
+    figure : str
+    value : int = field(init=False, repr=False, default=0)
+
+    def __post_init__(self):
+        object.__setattr__(self, 'value', value_list[figure_list.index(self.figure)])
 
     def __repr__(self) -> str:
-        return f'{self.figure}_{self.color}'
+        return f'{self.figure} _ {self.color}'
 
 class Deck:
     """A class that creates a deck of cards from a list of figures and colors.
@@ -75,33 +79,15 @@ Class value (int) returns the value of the player's cards.
     """
     def __init__(self, name) -> None:
         self.stored_cards = []
-        self.deck = Deck()
+        self.deck = Game.deck
         self.name = name
 
     def __int__(self) -> int:
-        return self.get_cards_value()
-
-    def first_run(self):
-        """The course of the player's first turn"""
-
-        self.stored_cards.append(self.deck.get_card())
-        self.stored_cards.append(self.deck.get_card())
+        return Game.calculate_card_value(self)
 
     def draw_card(self):
         """The corse of player's next turn"""
         self.stored_cards.append(self.deck.get_card())
-
-    def get_cards_value(self):
-        """Calculate value of player's cards
-
-        Returns:
-            int: sum values of player's catds
-        """
-        cards_value = sum([card.value for card in self.stored_cards])
-        if [True for card in self.stored_cards if card.figure.upper() == 'AS'] and cards_value < 21:
-            cards_value +=10
-
-        return cards_value
 
 class Game:
     """The class that carries out the course of the game
@@ -109,19 +95,21 @@ class Game:
     Args:
         Player class instances for the dealer and the user
     """
-
+    deck = Deck()
     def __init__(self, croupier, human) -> None:
-        self.croupier = croupier
-        self.player = human
-        self.player.deck = self.croupier.deck
+        self.croupier : Player = croupier
+        self.human : Player = human
 
     def first_run(self):
-        """The course of the first round of the game"""
-        self.player.first_run()
-        if int(self.player) == 21:
-            raise BlackJack('Black Jack! Wygrywasz')
-        self.croupier.first_run()
-        return self.player.stored_cards, self.croupier.stored_cards[0]
+        """The course of the first round of the game
+
+        Returns:
+            tupe(list[Card]): collected player's cards, one of the dealer's cards
+        """
+        for player in [self.human, self.croupier]:
+            player.draw_card()
+            player.draw_card()
+        return self.human.stored_cards, self.croupier.stored_cards[0]
 
     def player_run(self):
         """The player's next turns
@@ -129,10 +117,10 @@ class Game:
         Returns:
             list[Card]: player's stored cards
         """
-        self.player.draw_card()
-        if int(self.player) > 21:
-            raise OverTwentyOne(self.player)
-        return self.player.stored_cards
+        self.human.draw_card()
+        if int(self.human) > 21:
+            raise OverTwentyOne(self.human)
+        return self.human.stored_cards
 
     def croupier_run(self):
         """The croupier's next turn
@@ -146,33 +134,52 @@ class Game:
                 raise OverTwentyOne(self.croupier)
         return self.croupier.stored_cards
 
+    @staticmethod
+    def calculate_card_value(player : Player):
+        """calculating player's stored cards
+
+        Args:
+            player (Player): instance of Player
+
+        Returns:
+            int: sum values of cards stored by player
+        """
+        cards_value = sum([card.value for card in player.stored_cards])
+        if [True for card in player.stored_cards if card.figure.upper() == 'AS']\
+         and cards_value <= 11:
+            cards_value +=10
+        if cards_value == 21:
+                raise BlackJack(f'Black Jack! {player.name} wygrywa')
+        return cards_value
+
 if __name__ == '__main__':
 
-    player = Player('Gracz')
+    gracz = Player('Gracz')
     krupier = Player('Krupier')
-    game = Game(krupier, player)
+    game = Game(krupier, gracz)
     try:
         player_cards, krupier_card = game.first_run()
     except BlackJack as message:
         print(message)
-        print(player.stored_cards)
+        print(gracz.stored_cards)
     else:
         try:
-            print(f'Masz {player_cards} {int(player)}, krupier ma {krupier_card} {int(krupier)}')
+            print(f'Masz {player_cards} {int(gracz)}, krupier ma {krupier_card} {int(krupier)}')
             draw_next = input('dobierasz kartę? (t/n) ')
         except OverTwentyOne as message:
             print(message)
-            print(player.stored_cards)
+            print(gracz.stored_cards)
         else:
             try:
                 while draw_next == 't':
-                    print(f'Masz {game.player_run()} , {int(player)} ')
+                    print(f'Masz {game.player_run()} , {int(gracz)} ')
                     draw_next = input('dobierasz kartę? (t/n) ')
 
                 print(f'krupier ma {game.croupier_run()} , {int(krupier)} ')
-                if int(krupier) > int(player):
+                if int(krupier) > int(gracz):
                     print('Przegrywasz')
                 else:
                     print('WYGRYWASZ')
             except OverTwentyOne as message:
                 print(message)
+                print(gracz.stored_cards)
